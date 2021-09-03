7edc652f5c3f6dd9ed6dbed538a934154a422115
AndreeaDamian <89774781+AndreeaDamian@users.noreply.github.com>
2021-09-03 12:14:05 +0300

    #6; login; updates

--- pages/login.php
+++ pages/login.php
@@ -1,58 +1,65 @@
 <?php
-    require_once('../common.php');
-    session_start();
 
-    if(isset($_POST['login'])) {
+require_once '../common.php';
 
-        $email = strip_tags($_POST['email']);
-        $password = strip_tags($_POST['password']);
-        if($email == '' || $password == '')  echo 'Login failed! Email or password incorrect!';
-        if($email == ADMIN_USERNAME && $password == ADMIN_PASSWORD) {
-            if(!isset($_SESSION['login'])) {
+$error = [];
+
+if (isset($_POST['login'])) {
+    $email = strip_tags($_POST['email']);
+    $password = strip_tags($_POST['password']);
+    if (empty($email) || empty($password)) {
+        $error['login'] = translate('Login failed! Email or password incorrect!');
+    } else {
+        if ($email == ADMIN_USERNAME && $password == ADMIN_PASSWORD) {
+            if (!isset($_SESSION['login'])) {
                 $_SESSION['login'] = 'Admin is logged!';
             }
             header('Location: index.php');
+            exit;
         } else {
-            echo 'Login failed! Email or password incorrect!';
+            $error['login'] = translate('Login failed! Email or password incorrect!');
         }
     }
+}
 
-    if(isset($_GET['logout'])) {
-        unset($_SESSION['login']);
-        header('Location: login.php');
-    }
+if (isset($_GET['logout'])) {
+    unset($_SESSION['login']);
+    header('Location: login.php');
+    exit;
+}
 
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Shop</title>
-    <link rel="stylesheet" href="../assets/style.css">
-</head>
-<body>
-<?php include("header.php"); ?>
-    <section>
-        <div class="login">
-            <form action="login.php" method="POST">
-
-                <input type="hidden" name="login" value="true" required>
-                <div>
-                    <span>Email</span>
-                    <input type="email" name="email" required>
-                </div>
-                <div>
-                    <span>Password</span>
-                    <input type="password" name="password" required>
-                </div>
-                <button type="submit">Login</button>
-            </form>
-        </div>
-    </section>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Shop - <?= translate('Login') ?></title>
+        <link rel="stylesheet" href="../assets/style.css">
+    </head>
+    <body>
+        <?php require_once 'header.php'; ?>
+        <section>
+            <div class="login">
+                <form action="login.php" method="POST">
+                    <input type="hidden" name="login" value="true" required>
+                    <div>
+                        <span><?= translate('Email') ?></span>
+                        <input type="email" name="email">
+                    </div>
+                    <div>
+                        <span><?= translate('Password') ?></span>
+                        <input type="password" name="password">
+                    </div>
+                    <button type="submit"><?= translate('Login') ?></button>
+                </form>
+            </div>
+            <div>
+                <p style="text-align: center; color: red"><?= isset($error['login']) ? $error['login'] : '' ?></p>
+            </div>
+        </section>
+    </body>
 </html>
