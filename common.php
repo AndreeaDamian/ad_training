e1402e5f2f5893adb8d190245370625a108c1159
AndreeaDamian <89774781+AndreeaDamian@users.noreply.github.com>
2021-09-06 17:29:28 +0300

    code style

--- common.php
+++ common.php
@@ -1,122 +1,122 @@
 <?php
 
-    require_once "config.php";
+require_once "config.php";
 
-    session_start();
+session_start();
 
-    function connect()
-    {
-        $servername = DBHOST;
-        $username = DBUSER;
-        $password = DBPWD;
-        $database = DBNAME;
+function connect()
+{
+    $servername = DBHOST;
+    $username = DBUSER;
+    $password = DBPWD;
+    $database = DBNAME;
 
-        try {
-            $conn = new PDO(
-                "mysql:host=$servername;dbname=$database",
-                $username,
-                $password
-            );
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    try {
+        $conn = new PDO(
+            "mysql:host=$servername;dbname=$database",
+            $username,
+            $password
+        );
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-            return $conn;
-        } catch (PDOException $e) {
-            throw new PDOException( 'Unable to connect to database', 0, $e);
-        }
+        return $conn;
+    } catch (PDOException $e) {
+        throw new PDOException( 'Unable to connect to database', 0, $e);
     }
+}
 
-    function unauthenticated()
-    {
-        if (!isset($_SESSION['login'])) {
-            header('Location: login.php');
-            exit;
-        }
+function unauthenticated()
+{
+    if (!isset($_SESSION['login'])) {
+        header('Location: login.php');
+        exit;
     }
+}
 
-    function uploadImage($file)
-    {
-        $directoryPath = '../uploads/';
-        $filePath = $directoryPath . basename($file['name']);
+function uploadImage($file)
+{
+    $directoryPath = '../uploads/';
+    $filePath = $directoryPath . basename($file['name']);
+    $uploadOk = 1;
+
+    $check = getimagesize($file['tmp_name']);
+    if($check !== false) {
         $uploadOk = 1;
+    } else {
+        $error =  translate('File is not an image.');
+        $uploadOk = 0;
+    }
 
-        $check = getimagesize($file['tmp_name']);
-        if($check !== false) {
-            $uploadOk = 1;
-        } else {
-            $error =  translate('File is not an image.');
-            $uploadOk = 0;
-        }
-
-        if ($uploadOk == 0) {
-            $error = translate('Sorry, your file was not uploaded.');
-        } else {
-            if (!is_dir($directoryPath)) mkdir($directoryPath, 0755);
-            if (move_uploaded_file($file['tmp_name'], $filePath)) {
-                return $directoryPath.basename($file['name']);
-            }
+    if ($uploadOk == 0) {
+        $error = translate('Sorry, your file was not uploaded.');
+    } else {
+        if (!is_dir($directoryPath)) mkdir($directoryPath, 0755);
+        if (move_uploaded_file($file['tmp_name'], $filePath)) {
+            return $directoryPath.basename($file['name']);
         }
     }
+}
 
-    function getOrderedProducts($orderID)
-    {
-        $query = connect()->prepare("
-            SELECT *
-            FROM order_product
-            INNER JOIN products ON order_product.product_id=products.id 
-            WHERE order_id='$orderID'
-        ");
-        $query->execute();
-        return $query->fetchAll();
-    }
+function getOrderedProducts($orderID)
+{
+    $query = connect()->prepare("
+        SELECT *
+        FROM order_product
+        INNER JOIN products ON order_product.product_id=products.id 
+        WHERE order_id='$orderID'
+    ");
+    $query->execute();
+    return $query->fetchAll();
+}
 
-    function translate($string)
-    {
-        $strings = [
-            'Title'         => 'Nume produs',
-            'Description'   => 'Descriere',
-            'Price'         => 'Pret',
-            'Action'        => 'Actiune',
-            'ADD TO CART'   => 'ADAUGA IN COS',
-            'Login'         => 'Autentificare',
-            'Home'          => 'Acasa',
-            'Cart'          => 'Cosul meu',
-            'Products'      => 'Produse',
-            'Logout'        => 'Dezautentificare',
-            'Remove'        => 'Sterge',
-            'There are no products in cart!' => 'Nu exista produse in cos!',
-            'Name'          => 'Nume',
-            'Contact Details' => 'Detalii de contact',
-            'Comments'      => 'Comentarii',
-            'Go to index'   => 'Catre Acasa',
-            'Checkout'      => 'Trimite comanda',
-            'Shop Order'    => 'Comanda magazin',
-            'Order Date'    => 'Data comanda',
-            'Product Name'  => 'Nume produs',
-            'Image'         => 'Imagine',
-            'Email'         => 'Email',
-            'Password'      => 'Parola',
-            'Login failed! Email or password incorrect!' => 'Autentificare nereusita! Email sau parola incorecta!',
-            'Add product'   => 'Adauga produs',
-            'Actions'       => 'Actiuni',
-            'EDIT'          => 'EDITEAZA',
-            'DELETE'        => 'STERGE',
-            'Nr'            => 'Nr',
-            'Sorry, there was an error uploading your file.' => 'Ne pare rau! A aparut o eroare la incarcarea fisierului',
-            'Sorry, your file was not uploaded.' => 'Ne pare rau! Fisierul nu a fost incarcat!',
-            'File is not an image.' => 'Fisierul nu este o imagine!',
-            'Add'           => 'Adauga',
-            'Edit'          => 'Editeaza',
-            'Date'          => 'Data',
-            'Customer Details' => 'Detalii client',
-            'Purchased products' => 'Produse Comandate',
-            'Total'         => 'Total',
-            'Orders'        => 'Comenzi',
-            'DETAILS'       => 'DETALII',
-            'Order Nr'      => 'Comanda Nr',
-        ];
+function translate($string)
+{
+    $strings = [
+        'Title'         => 'Nume produs',
+        'Description'   => 'Descriere',
+        'Price'         => 'Pret',
+        'Action'        => 'Actiune',
+        'ADD TO CART'   => 'ADAUGA IN COS',
+        'Login'         => 'Autentificare',
+        'Home'          => 'Acasa',
+        'Cart'          => 'Cosul meu',
+        'Products'      => 'Produse',
+        'Logout'        => 'Dezautentificare',
+        'Remove'        => 'Sterge',
+        'There are no products in cart!' => 'Nu exista produse in cos!',
+        'Name'          => 'Nume',
+        'Contact Details' => 'Detalii de contact',
+        'Comments'      => 'Comentarii',
+        'Go to index'   => 'Catre Acasa',
+        'Checkout'      => 'Trimite comanda',
+        'Shop Order'    => 'Comanda magazin',
+        'Order Date'    => 'Data comanda',
+        'Product Name'  => 'Nume produs',
+        'Image'         => 'Imagine',
+        'Email'         => 'Email',
+        'Password'      => 'Parola',
+        'Login failed! Email or password incorrect!' => 'Autentificare nereusita! Email sau parola incorecta!',
+        'Add product'   => 'Adauga produs',
+        'Actions'       => 'Actiuni',
+        'EDIT'          => 'EDITEAZA',
+        'DELETE'        => 'STERGE',
+        'Nr'            => 'Nr',
+        'Sorry, there was an error uploading your file.' => 'Ne pare rau! A aparut o eroare la incarcarea fisierului',
+        'Sorry, your file was not uploaded.' => 'Ne pare rau! Fisierul nu a fost incarcat!',
+        'File is not an image.' => 'Fisierul nu este o imagine!',
+        'Add'           => 'Adauga',
+        'Edit'          => 'Editeaza',
+        'Date'          => 'Data',
+        'Customer Details' => 'Detalii client',
+        'Purchased products' => 'Produse Comandate',
+        'Total'         => 'Total',
+        'Orders'        => 'Comenzi',
+        'DETAILS'       => 'DETALII',
+        'Order Nr'      => 'Comanda Nr',
+    ];
 
-        if (!isset($strings[$string])) return $string;
-        return $strings[$string];
-    }
+    if (!isset($strings[$string])) return $string;
+    return $strings[$string];
+}
 
 ?>
