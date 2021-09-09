7b4c4e657759a23bac9af431781b9c1dbc4ef59e
AndreeaDamian <89774781+AndreeaDamian@users.noreply.github.com>
2021-09-09 17:51:45 +0300

    #4; index; translate

--- pages/index.php
+++ pages/index.php
@@ -35,7 +35,7 @@ if (isset($_POST['product_id'])) {
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>SHOP</title>
+        <title><?= translate('Shop') ?></title>
         <link rel="stylesheet" href="../assets/style.css">
     </head>
     <body>
